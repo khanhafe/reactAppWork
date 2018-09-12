c967c13162266e8852af35507f76ba650544c197
khanhafe <khanhafe@gmail.com>
2018-09-12 17:38:47 +0530

    ecommerce

---
+++ src/containers/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import { Provider } from "react-redux";
+
+import Shelf from "../components/shelf/Shelf";
+import Footer from "../components/Footer";
+import FloatCart from "../components/floatcart/FloatCart";
+
+import store from "../store";
+import Corner from "../components/github/Corner";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Corner />
+          <main>
+            <Shelf />
+          </main>
+          <Footer />
+          <FloatCart />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
